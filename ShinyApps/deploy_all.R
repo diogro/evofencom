50764ec05864a5186e5665f2b5a7bfb61e7a6704
Diogo Melo <diogro@gmail.com>
2026-05-10 22:14:04 -0300

    secret!

--- ShinyApps/deploy_all.R
+++ ShinyApps/deploy_all.R
@@ -1,14 +1,16 @@
 if(!require(rsconnect)){install.packages("rsconnect"); library(rsconnect)}
-rsconnect::setAccountInfo(name='curso-genevol',
-                          token='95346A5518D36EC95824A1AC86E965D1',
-                          secret='secret_key')
+rsconnect::setAccountInfo(name='damelo', 
+                          token='95346A5518D36EC95824A1AC86E965D1', 
+                          secret='SecretKey')
 
 # Deploy
 if(!require(here)){install.packages("here"); library(here)}
-rsconnect::deployApp(here("Eq-de-Lande"))
-rsconnect::deployApp(here("Superficie-Adaptativa"))
+rsconnect::deployApp(here("ShinyApps/Eq-de-Lande"))
+rsconnect::deployApp(here("ShinyApps/Superficie-Adaptativa"))
+rsconnect::deployApp(here("ShinyApps/AlleleEffects"))
 
 # Run local
 if(!require(shiny)){install.packages("shiny"); library(shiny)}
-runGitHub("genetica-quantitativa", "Curso-Genevol", subdir = "Eq-de-Lande")
-runGitHub("genetica-quantitativa", "Curso-Genevol", subdir = "Superficie-Adaptativa")
+runGitHub("diogro", "evofencom", subdir = "ShinyApps/Eq-de-Lande")
+runGitHub("diogro", "evofencom", subdir = "ShinyApps/Superficie-Adaptativa")
+
